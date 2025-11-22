7c31dbe2a00dff994542f126329dfe5a5ec82df8
salads-source <ron52086@gmail.com>
2025-11-23 02:07:33 +0800

    feat: add test endpoint

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world")
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.Run(":8080") // loaclhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
 }
