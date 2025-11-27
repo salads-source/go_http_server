30b099cdb4babff263f9674d4c3a464a46348573
salads-source <ron52086@gmail.com>
2025-11-27 19:39:14 +0800

    feat: add password hashing

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/salads-source/go_http_server/db"
+import (
+	"github.com/salads-source/go_http_server/db"
+	"github.com/salads-source/go_http_server/utils"
+)
 
 type User struct {
 	ID       int64
@@ -18,7 +21,13 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassWord(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 
 	if err != nil {
 		return err
