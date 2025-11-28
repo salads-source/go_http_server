6560fc28e8f325ce3c97afecdfe26c6c8a834afb
salads-source <ron52086@gmail.com>
2025-11-29 01:38:29 +0800

    feat: add register for event functionality

--- db/db.go
+++ db/db.go
@@ -53,4 +53,20 @@ func createTables() {
 	if err != nil {
 		panic("Could not create event table.")
 	}
+
+	createRegistrationsTable := `
+	CREATE TABLE IF NOT EXISTS registrations (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		event_id INTEGER,
+		user_id INTEGER,
+		FOREIGN KEY(event_id) REFERENCES events(id),
+		FOREIGN KEY(user_id) REFERENCES users(id)
+	);
+	`
+
+	_, err = DB.Exec(createRegistrationsTable)
+
+	if err != nil {
+		panic("Could nto create registrations table.")
+	}
 }
