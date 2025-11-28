0b0010b1cce3e2c0f09678a8a7d6c57134abb157
salads-source <ron52086@gmail.com>
2025-11-29 01:54:30 +0800

    feat: add cancel registration for event functionality

--- routes/register.go
+++ routes/register.go
@@ -34,4 +34,24 @@ func registerForEvent(context *gin.Context) {
 	context.JSON(http.StatusCreated, gin.H{"message": "User registered for event successfully"})
 }
 
-func cancelRegistration(context *gin.Context) {}
+func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event Id"})
+		return
+	}
+
+	var event models.Event
+	event.ID = eventId
+
+	err = event.CancelRegistration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not register user for event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Registration for event cancelled successfully"})
+}
